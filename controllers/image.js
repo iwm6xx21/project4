4b3aac730c68917de15592b7c54f57557db59cc4
ispoon21 <isaiahweathersp@gmail.com>
2022-03-20 17:47:32 -0600

    show route created. working on styling and editor functionaility

--- controllers/image.js
+++ controllers/image.js
@@ -11,26 +11,27 @@ router.get('/upload',(req, res) => {
     res.render('upload')
 })
 
-
 // post route for uploading image to the editor
 router.post('/editor', async (req, res) => {
     const uploads = new Image(req.body)
     await uploads.save()
     console.log(uploads)
-    res.redirect('/editor');
+    res.redirect(`/${uploads.id}`);
 })
 
-// router.get('/editor', (req, res) => {
-//     const uploads = Image.find({})
-//     console.log(uploads)
-//         res.render('editor', {uploads})
-// })
 
 router.get("/editor", (req, res) => {
     Image.find({},(err,images) => {
         res.render('editor', {images})
     });
 })
+
+// show route
+router.get('/:id', async (req, res) => {
+    const images = await Image.findById(req.params.id)
+        res.render('show', {images})
+})
+
 
 
 module.exports = router
